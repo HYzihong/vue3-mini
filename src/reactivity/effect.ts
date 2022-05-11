f14ca94fdb1b3e2eba96a64103aef07510086c63
hy <zihong2020@foxmail.com>
2022-05-11 22:50:53 +0800

    fix(reactivity):fix effect track

--- src/reactivity/effect.ts
+++ src/reactivity/effect.ts
@@ -3,7 +3,7 @@
  * @Date: 2022-05-10 22:41:20
  * @LastEditors: hy
  * @Description: 
- * @LastEditTime: 2022-05-10 23:49:04
+ * @LastEditTime: 2022-05-11 22:47:43
  * @FilePath: /vue3-mini/src/reactivity/effect.ts
  * Copyright 2022 hy, All Rights Reserved. 
  * 仅供学习使用~
@@ -13,13 +13,29 @@ class ReactiveEffect {
   private _fn: any
   constructor(fn: any) {
     this._fn = fn
-
   }
   run() {
-    activateEffect = this //  activateEffect  => ReactiveEffect
+    activeEffect = this //  activeEffect  => ReactiveEffect
     // 执行传进来的fn
     this._fn()
   }
+}
+
+const targetMap = new Map()
+export function track(target: any, key: any) {
+  let depsMap = targetMap.get(target)
+  if (!depsMap) {
+    // 初始化
+    depsMap = new Map()
+    targetMap.set(target, depsMap)
+  }
+  let dep = depsMap.get(key)
+  if (!dep) {
+    // 初始化
+    dep = new Set()
+    depsMap.set(key, dep)
+  }
+  dep.add(activeEffect)
 }
 
 export function trigger(target: any, key: any) {
@@ -31,25 +47,7 @@ export function trigger(target: any, key: any) {
   }
 }
 
-const targetMap = new Map()
-export function track(target: any, key: any) {
-  let depsMap = targetMap.get(target)
-  if (depsMap) {
-    // 初始化
-    depsMap = new Map()
-    targetMap.set(target, depsMap)
-  }
-  let dep = depsMap.get(key)
-  if (dep) {
-    // 初始化
-    dep = new Set()
-    depsMap.set(key, dep)
-
-  }
-  dep.add(activateEffect)
-
-}
-let activateEffect: ReactiveEffect
+let activeEffect: ReactiveEffect
 export function effect(fn: any) {
   const _effect = new ReactiveEffect(fn)
 
--- src/reactivity/reactive.ts
+++ src/reactivity/reactive.ts
@@ -3,7 +3,7 @@
  * @Date: 2022-05-09 23:26:19
  * @LastEditors: hy
  * @Description: 
- * @LastEditTime: 2022-05-10 23:37:40
+ * @LastEditTime: 2022-05-11 22:48:00
  * @FilePath: /vue3-mini/src/reactivity/reactive.ts
  * Copyright 2022 hy, All Rights Reserved. 
  * 仅供学习使用~
@@ -17,14 +17,12 @@ export default function reactive(raw: any): any {
       // key fpp
       // target {foo:1}
       const res = Reflect.get(target, key)
-      // TODO 依赖收集
       track(target, key)
       return res
     },
     set(target, key, value) {
       const res = Reflect.set(target, key, value)
       trigger(target, key)
-      // TDOD 收集依赖
       return res
     }
   })
